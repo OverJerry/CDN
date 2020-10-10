4561df1ef2137e99007fdfbef20cf2163eb8097c
OverJerry <46861729+OverJerry@users.noreply.github.com>
2020-10-10 20:52:25 +0800

    new model

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/OverJerry/CDN@1.4/live2d/";
+const live2d_path = "http://127.0.0.1/CDN/live2d/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			cdnPath: "https://cdn.jsdelivr.net/gh/OverJerry/CDN@1.4/api/",
+			cdnPath: "http://127.0.0.1/CDN/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});

bb3679ebed1fdbf47c0a5ff16b9666d92386d0b3
OverJerry <46861729+OverJerry@users.noreply.github.com>
2020-12-31 12:37:07 +0800

    Update waifu-tips.js

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -181,8 +181,8 @@ function loadWidget(config) {
 	}
 
 	(function initModel() {
-		// let modelId = localStorage.getItem("modelId"),
-			// modelTexturesId = localStorage.getItem("modelTexturesId");
+		let modelId = localStorage.getItem("modelId"),
+			modelTexturesId = localStorage.getItem("modelTexturesId");
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			modelId = 1; // 模型 ID
